30920d655883df2b2c142081e2e60629ac741d1b
julio <juliocmp@unochapeco.edu.br>
2025-05-14 14:06:52 -0300

    first commit

---
+++ src/app/admin/categories/page.js
@@ -0,0 +1,10 @@
+import { Button, HStack } from "@chakra-ui/react"
+
+const Categories = () => {
+  return (
+    <HStack>
+      <Button>Click me</Button>
+      <Button>Click me</Button>
+    </HStack>
+  )
+}
--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -1,6 +1,6 @@
 import { Provider } from "@/components/ui/provider"
 
-export default function RootLayout({children}) {
+export default function RootLayout({ children }) {
   return (
     <html suppressHydrationWarning>
       <body>
